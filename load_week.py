8e0c7d0fa5f6aad5b06272e9a8de45c7befdd579
Carlos Eduardo da Silva Martins <cmartins@mordor.mordor>
2016-12-17 12:33:03 -0200

    Showing only one unpredicted game at time for the user; creating week loader for users; using callback to show other games

---
+++ load_week.py
@@ -0,0 +1,62 @@
+import http.client, urllib.error, sys, urllib.parse, json, os
+
+
+def create_directory(path):
+    if not (os.path.isdir(path)):
+        os.mkdir(path)
+
+
+def create_week_base_file(week, data):
+    create_directory(week)
+
+    with open(week + '/week-' + week + '.json', 'w') as data_file:
+        json.dump(data, data_file)
+
+
+def create_week_files_for_users(week):
+    users_list = ['csmartins', 'douglasrhx', 'lbalabram', 'leozinho420', 'guru_mikonha', 'Carekaled', 'Thiago']
+
+    create_directory(week)
+
+    with open(week + '/week-' + week + '.json') as data_file:
+        games = json.load(data_file)
+        users_predict = []
+        for game in games:
+            d = {'GameKey': game['GameKey'], 'AwayTeam': game['AwayTeam'], 'HomeTeam': game['HomeTeam'], 'predicted': False}
+            users_predict.append(d)
+
+        for user in users_list:
+            with open(week + '/' + user + '.json', 'w') as user_games:
+                json.dump(users_predict, user_games)
+
+
+def do_main():
+    if len(sys.argv) < 3:
+        print("Missing parameters")
+        print("Expected: python load_week.py <season> <week>")
+        return
+
+    season = sys.argv[1]
+    week = sys.argv[2]
+
+    headers = {
+        # Request headers
+        'Ocp-Apim-Subscription-Key': '78dc11b4e6294b30a5fd268fc26c3e8c',
+    }
+
+    try:
+        conn = http.client.HTTPSConnection('api.fantasydata.net')
+        conn.request("GET", "/v3/nfl/scores/JSON/ScoresByWeek/{}/{}?%s".format(season, week), "", headers)
+        response = conn.getresponse()
+        data = response.read().decode('utf-8')
+
+        data = json.loads(data)
+        create_week_base_file(week, data)
+        create_week_files_for_users(week)
+
+        conn.close()
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+if __name__ == '__main__':
+    do_main()
--- main.py
+++ main.py
@@ -1,38 +1,64 @@
 import telepot
 import asyncio
 import telepot.aio
-import random
-#from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton
+import telepot.aio.helper
+import sys
+import json
+from telepot import glance, message_identifier
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton
 from telepot.aio.delegate import (per_chat_id, create_open, pave_event_space, include_callback_query_chat_id)
 
+class NFLPredict(telepot.aio.helper.ChatHandler):
 
-async def on_chat_message(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
+    def __init__(self, *args, **kwargs):
+        super(NFLPredict, self).__init__(*args, **kwargs)
+        self.current_season = '2016'
+        self.current_week = 15
 
-    if content_type != 'text':
-        return
+    async def _show_unpredicted_match(self, chat_id, username):
+        with open(str(self.current_week) + '/' + username + '.json') as data:
+            week = json.load(data)
+            for game in week:
+                if not game['predicted']:
+                    keyboard = InlineKeyboardMarkup(inline_keyboard=[[
+                        InlineKeyboardButton(text=game['HomeTeam'], callback_data='casa'),
+                        InlineKeyboardButton(text='Empate', callback_data='empate'),
+                        InlineKeyboardButton(text=game['AwayTeam'], callback_data='visitante'),]])
+                    await self.bot.sendMessage(chat_id, 'Week ' + str(self.current_week), reply_markup=keyboard)
+                    return
 
-    command = msg['text'].lower()
-		
-    if command == '/predict':
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[[
-                    InlineKeyboardButton(text='Time de casa', callback_data='casa'),
-                    InlineKeyboardButton(text='Empae', callback_data='empate'),
-                    InlineKeyboardButton(text='Time visitante', callback_data='visitante'),]])
-        await bot.sendMessage(chat_id, 'Week X', reply_markup=keyboard)
-		
-async def on_callback_query(msg):
-	query_id, from_id, query_data = glance(msg, flavor='callback_query')
+    async def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
 
-	print(query_data)
+        if content_type != 'text':
+            return
 
-TOKEN = '302874617:AAEG99KVuWLo3SlN9WVxZTR8s496Ftuylg0'
-bot = telepot.aio.Bot(TOKEN, [include_callback_query_chat_id(pave_event_space())(per_chat_id(), create_open)])
-answerer = telepot.aio.helper.Answerer(bot)
+        command = msg['text'].lower()
+        if command == '/predict':
+            await self._show_unpredicted_match(chat_id, msg['from']['username'])
+
+    async def on_callback_query(self, msg):
+        query_id, from_id, query_data = glance(msg, flavor='callback_query')
+
+        await self._show_unpredicted_match(from_id, msg['from']['username'])
+#
+# def do_main():
+# 	if len(sys.argv) < 2:
+# 		print("Insuficient number of arguments.")
+# 		print("Expected: python main.py <TOKEN>")
+# 		return
+
+TOKEN = sys.argv[1]
+bot = telepot.aio.DelegatorBot(TOKEN, [
+    include_callback_query_chat_id(
+        pave_event_space())(
+            per_chat_id(types=['private', 'group']), create_open, NFLPredict, timeout=60)])
 
 loop = asyncio.get_event_loop()
 loop.create_task(bot.message_loop())
 
 print("Listening...")
 loop.run_forever()
+
+# if __name__ == "__main__":
+#     do_main()
